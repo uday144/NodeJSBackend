128a7330520526e3c0506cf08875596830df5be7
Uday Singh <uday144@gmail.com>
2024-03-29 07:58:25 +1100

    http://localhost:5000/ - response hello

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+
+app.get("/", (req, res) =>{
+    res.send("Hello");
+});
+
 app.listen(5000, ()=>{
     console.log("Server started on port no. " + 5000);
 });
